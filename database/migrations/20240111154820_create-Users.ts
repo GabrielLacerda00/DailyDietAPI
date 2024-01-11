6185c38298e45d3dd357aa53c0328ecdae627c4e
Gabriel Lacerda <gabriellacerda@MacBook-Air-de-Gabriel.local>
2024-01-11 13:48:16 -0300

    creating table meals and update table users

--- database/migrations/20240111154820_create-Users.ts
+++ database/migrations/20240111154820_create-Users.ts
@@ -1,10 +1,19 @@
-import type { Knex } from "knex";
+import { table } from 'console'
+import type { Knex } from 'knex'
 
-
+// Método que define o que minha migration vai fazer
 export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('users', (table) => {
+    table.uuid('id').primary()
+    table.text('session_id').notNullable().unique()
+    table.text('name').notNullable()
+    table.text('email').notNullable().unique()
+    table.date('date').notNullable()
+    table.timestamps(true, true)
+  })
 }
 
-
+// Desfaz o que o método up fez - rollback
 export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('users')
 }
-

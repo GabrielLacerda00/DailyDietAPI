ebddab72e93ecfcb699cf2cd9edf707ca63a214d
Gabriel Lacerda <gabriellacerda@MacBook-Air-de-Gabriel.local>
2024-01-11 13:59:58 -0300

    testing creating  users

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,20 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import crypto from 'crypto'
 
 const app = fastify()
 
 app.get('/test', async () => {
-  const test = await knex('sqlite_schema').select('*')
+  const test = await knex('users')
+    .insert({
+      id: crypto.randomUUID(),
+      session_id: '12345678910',
+      name: 'User Test',
+      email: 'gabtes@gmail.com',
+      date: new Date(),
+    })
+    .returning('*')
+
   return test
 })
 

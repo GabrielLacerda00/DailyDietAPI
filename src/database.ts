4c1c95f1739cbb10ec0040639c4907f015f916a1
Gabriel Lacerda <gabriellacerda@MacBook-Air-de-Gabriel.local>
2024-01-11 12:49:11 -0300

    creating table Users in database

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,14 @@
-import { knex as setUpKnex } from 'knex'
+import { knex as setUpKnex, Knex } from 'knex'
 
-export const knex = setUpKnex({
+export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
     filename: './database/app.db',
   },
   useNullAsDefault: true,
-})
+  migrations: {
+    extension: 'ts',
+    directory: './database/migrations',
+  },
+}
+export const knex = setUpKnex(config)

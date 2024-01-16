dd2106e8e70cb0db97b3cdff10bc67eddd55227f
Gabriel Lacerda <gabriellacerda@MacBook-Air-de-Gabriel.local>
2024-01-15 21:04:09 -0300

    routes: User metrics

--- src/routes/mealsRoutes.ts
+++ src/routes/mealsRoutes.ts
@@ -168,4 +168,53 @@ export async function mealsRoutes(app: FastifyInstance) {
       return reply.status(204).send()
     },
   )
+
+  app.get(
+    '/:userId/metrics',
+    { preHandler: [checkSessionIdExists] },
+    async (request, reply) => {
+      const paramsSchema = z.object({
+        userId: z.string(),
+      })
+      // verifico o userId e o mealId
+      const { userId } = paramsSchema.parse(request.params)
+
+      // Descobrir o total de meals da dieta
+      const totalMealsOnDiet = await knex('meals')
+        .where({ userId, is_on_diet: true })
+        .count('id', { as: 'total' })
+        .first()
+      // Descobrir o total de meals fora da dieta
+      const totalMealsOffDiet = await knex('meals')
+        .where({ userId, is_on_diet: false })
+        .count('id', { as: 'total' })
+        .first()
+      const totalMeals = await knex('meals')
+        .where({ userId })
+        .orderBy('date', 'desc')
+
+      const { bestOnDietSequence } = totalMeals.reduce(
+        (acc, meal) => {
+          if (meal.is_on_diet) {
+            acc.currentSequence += 1
+          } else {
+            acc.currentSequence = 0
+          }
+
+          if (acc.currentSequence > acc.bestOnDietSequence) {
+            acc.bestOnDietSequence = acc.currentSequence
+          }
+
+          return acc
+        },
+        { bestOnDietSequence: 0, currentSequence: 0 },
+      )
+      return reply.send({
+        totalMeals: totalMeals.length,
+        totalMealsOnDiet: totalMealsOnDiet?.total,
+        totalMealsOffDiet: totalMealsOffDiet?.total,
+        bestOnDietSequence,
+      })
+    },
+  )
 }
